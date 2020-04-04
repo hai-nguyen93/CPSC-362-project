71076c7df8d880daf114c62840d8c327249906e8
lddang1762 <lddang1762@users.noreply.github.com>
2020-04-03 21:08:11 -0700

    Raise Button and Slider
    Clicking Raise will toggle the slider and confirm button on and the other 2 buttons off, and clicking the confirm button will all the playerRaise().

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,17 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
     Hand hand;
     GameController gc;
+    public GameObject slide;
+    public GameObject confirm;
+    public GameObject disableCall;
+    public GameObject disableFold;
 
     // Start is called before the first frame update
     void Start()
     {        
         hand = GetComponent<Hand>();
-        gc = FindObjectOfType<GameController>();
+        gc = FindObjectOfType<GameController>();        
+        slide.SetActive(false);
+        confirm.SetActive(false);
     }
 
     // Update is called once per frame
@@ -42,11 +49,36 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    public void RaiseClicked()
+    {
+        if (slide.activeSelf == true)
+        {
+            slide.SetActive(false);
+            confirm.SetActive(false);
+        }
+        else
+        {
+            slide.SetActive(true);
+            confirm.SetActive(true);
+        }
+        if (disableCall.activeSelf == true)
+        {
+            disableFold.SetActive(false);
+            disableCall.SetActive(false);
+        }
+        else
+        {
+            disableFold.SetActive(true);
+            disableCall.SetActive(true);
+        }
+    }
+
     public void PlayerRaise()
     {
         if (gc.gState == GameState.PlayerTurn)
         {
             Debug.Log("Player Raise");
+            
 
             hand.Raise();
             // do something
