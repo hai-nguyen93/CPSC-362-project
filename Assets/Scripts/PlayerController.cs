2c97b6ae6fd06acb632419bc6ace26c610f64f6f
austinh763 <58415160+austinh763@users.noreply.github.com>
2020-04-05 19:57:50 -0700

    Revert "added check button/function"
    This reverts commit f1e3b588dc85efa34fc66d0ced58f567f46db8d8.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,35 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
     Hand hand;
     GameController gc;
+    public GameObject slideObject;
+    Slider slider;
+    public GameObject confirm;
+    public GameObject disableCall;
+    public GameObject disableFold;
+    public GameObject raiseAmountText;
     int raiseAmount;
+
+    //public GameObject potText;
+    //public GameObject playerBankText;
+    //pot and player bank eventually accessed from other class/script. placeholders for now.
+    //int playerBank;
+    //int pot;
 
     // Start is called before the first frame update
     void Start()
     {        
         hand = GetComponent<Hand>();
-        gc = FindObjectOfType<GameController>();
+        gc = FindObjectOfType<GameController>();        
+        slideObject.SetActive(false);
+        slider = slideObject.GetComponent<Slider>();
+        slider.maxValue = 10000; // to be set to whatever current players bank is
+        confirm.SetActive(false);
+        raiseAmountText.SetActive(false);
+
+        //playerBank = 10000;
+        //playerBankText.GetComponent<Text>().text = "$" + playerBank;          
     }
 
     // Update is called once per frame
     void Update()
     {
         
-    }
-
-    public void PlayerCheck()
-    {
-        if(gc.gState == GameState.PlayerTurn)
-        {
-            Debug.Log("Player Check");
-            hand.Check();
-            //do something
-            gc.EndPlayerTurn();
-        }
     }
 
     public void PlayerFold()
@@ -54,15 +64,68 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    public void RaiseClicked()
+    {
+        //toggles UI elements on-click
+        if (slideObject.activeSelf == true)
+        {
+            slideObject.SetActive(false);
+            confirm.SetActive(false);
+            raiseAmountText.SetActive(false);
+        }
+        else
+        {
+            slideObject.SetActive(true);
+            confirm.SetActive(true);
+            raiseAmountText.SetActive(true);
+        }
+        if (disableCall.activeSelf == true)
+        {
+            disableFold.SetActive(false);
+            disableCall.SetActive(false);
+        }
+        else
+        {
+            disableFold.SetActive(true);
+            disableCall.SetActive(true);
+        }
+    }
+
+    public void sliderUpdate()
+    {
+        //updates Raise Amount Text based on slider location
+        Debug.Log("Slider Value Changed");
+
+        Text amount = raiseAmountText.GetComponent<Text>();
+        raiseAmount = (int)slider.value;
+        amount.text = "Amount: $" + raiseAmount;
+
+        Debug.Log("Raise amount = " + raiseAmount);
+    }
+
     public void PlayerRaise()
     {
         if (gc.gState == GameState.PlayerTurn)
         {
-            Debug.Log("Player Raise");
-            //need to grab raise amount somehow
-            hand.Raise(raiseAmount);
-            // do something
-            gc.EndPlayerTurn();
+            if(raiseAmount == 0)
+            {
+                Debug.Log("Raise Amount must be greater than $0");
+            }
+            else
+            {
+                Debug.Log("Player Raise");
+
+                //toggle buttons and text to original state
+                disableFold.SetActive(true);
+                disableCall.SetActive(true);
+                slideObject.SetActive(false);
+                confirm.SetActive(false);
+                raiseAmountText.SetActive(false);                          
+
+                hand.Raise(raiseAmount);
+                // do something
+                gc.EndPlayerTurn(); 
+            }
         }
     }
 }
